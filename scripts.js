f73679131c74aa666cc04bfbaf7a64e7e2aed738
Kosmobel <131285977+Kosmobel@users.noreply.github.com>
2023-07-04 19:30:54 +0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.nav');
+
+
+
+var res = window.matchMedia("(max-width: 575.98px)")
+if(res.matches){
+  menu.style.display = 'none';
+}
+
+
+
+menuBtn.addEventListener('click', function() {
+  if (menu.style.display === 'none') {
+    menu.style.display = 'grid';
+    menu.style.position = 'fixed';
+    menuBtn.style.background = "green"
+  } else {
+    menu.style.display = 'none';
+    menuBtn.style.background = "#333"
+  }
+});
